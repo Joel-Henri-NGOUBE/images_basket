8fc4e05168e2ec04374b62969a1516438d12753e
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2024-09-12 23:17:35 +0200

    Test avec un site de référence

--- api.js
+++ api.js
@@ -3,9 +3,31 @@ const app = express()
 const port = 3000
 
 const axios = require('axios');
+"".split()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  axios.get('https://lidl.fr')
+    .then(response => {
+      // console.log(response.data)
+      const imgTags = response.data.match(/<img.*?>|<img.*?\/>/g)
+      const srcAttributes = imgTags.map((str) => 
+        str.match(/src=".*?"/g).join("")
+      )
+      const src = srcAttributes.map((str) => str.split("\"")[1])
+      console.log(src)
+      const url = src.map((src) => 
+        src.startsWith("https") ? src : 'https://lidl.fr' + src
+      )
+      const newImg = url.map((url) => 
+        `<img src="${url}">`
+      )
+      console.log(url)
+      res.send(newImg.join("<br>"))
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  // res.send('Hello World!')
 })
 
 app.listen(port, () => {
