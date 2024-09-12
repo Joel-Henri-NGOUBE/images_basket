565fbfdf3d48e58880c70165b23c2a5593b20263
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2024-09-13 00:33:56 +0200

    Implémentation terminée

--- api.js
+++ api.js
@@ -3,33 +3,63 @@ const app = express()
 const port = 3000
 
 const axios = require('axios');
-"".split()
 
-app.get('/', (req, res) => {
-  axios.get('https://lidl.fr')
-    .then(response => {
-      // console.log(response.data)
-      const imgTags = response.data.match(/<img.*?>|<img.*?\/>/g)
-      const srcAttributes = imgTags.map((str) => 
-        str.match(/src=".*?"/g).join("")
-      )
-      const src = srcAttributes.map((str) => str.split("\"")[1])
-      console.log(src)
-      const url = src.map((src) => 
-        src.startsWith("https") ? src : 'https://lidl.fr' + src
-      )
-      const newImg = url.map((url) => 
-        `<img src="${url}">`
-      )
-      console.log(url)
-      res.send(newImg.join("<br>"))
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  // res.send('Hello World!')
+const searchUrls = []
+
+app.get('/getimages', (req, res) => {
+  
+  try{
+    const searchedUrl = req.query.url
+
+    if(!searchedUrl){
+      return res.json({"error": "Bad request", "message":  "No url to search"})
+    }
+    
+    if(!(searchUrls.indexOf(searchedUrl) >= 0)){
+      searchUrls.push(searchedUrl)
+    }
+
+    // Requesting the entered url
+    axios.get(searchedUrl)
+      .then(response => {
+        const imgTags = response.data?.match(/<img.*?>|<img.*?\/>/g)
+        
+        console.log(imgTags)
+
+        const srcAttributes = imgTags?.map((img) => 
+          img.match(/src=".*?"/g) ? img.match(/src=".*?"/g).join("") : img
+        )
+        
+        // The sources are allows at the same position because there is always a 'src="' in front 
+        const src = srcAttributes?.map((src) => src.split("\"")[1])
+        
+        const urls = src?.map((src) => 
+          src.startsWith("https") ? src : searchedUrl + src
+        )
+        
+        const newImgs = urls?.map((url) => 
+          `<img src="${url}">`
+        )
+  
+        res.send(newImgs?.join("<br>"))
+      })
+      .catch(error => {
+        console.log(error)
+      });
+  }
+  catch(error){
+    console.log(error)
+  }
+})
+
+app.get("/researches", (req, res) => {
+  try {
+    return res.json({"status": "OK", "researches": searchUrls})
+  } catch (error) {
+    console.error(error)
+  }
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`API listening on port ${port}`)
 })
